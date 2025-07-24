106cdb8bb080b90d665377a49804118db1768bf5
Pramila <prami321subramani@gmail.com>
2025-07-24 09:38:58 +0530

    add master javabasics1

---
+++ src/javaBasic1.java
@@ -0,0 +1,24 @@
+
+public class javaBasic1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// while loop
+				int i = 5;
+				while (i > 0) {
+					System.out.println(i);
+					System.out.println(i);
+					System.out.println(i);
+					System.out.println(i);// develop done
+					System.out.println(i);// develop done
+					System.out.println(i);// develop git demo
+					System.out.println(i);// develop git demo
+					
+					
+					i--;
+				}
+
+	}
+
+}
